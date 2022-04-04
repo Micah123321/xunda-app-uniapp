ab71a8e7c9367342709bb4e107a683ea91b64a35
micah <1569124799@qq.com>
2022-04-04 14:48:29 +0800

    初步搭建页面样式:首页

--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,6 +1,21 @@
 <template>
 	<view>
-		123
+		<uni-nav-bar statusBar title="xunda">
+
+			<block slot="left">
+				<view class="bar-right">
+					<icon type="search"></icon>
+				</view>
+			</block>
+			<block slot="right">
+				<view class="bar-left">
+					<uni-icons class="bar-search" size="25" type="search" />
+					<uni-icons class="bar-plus" size="25" type="plus" />
+
+				</view>
+			</block>
+		</uni-nav-bar>
+
 	</view>
 </template>
 
@@ -8,15 +23,21 @@
 	export default {
 		data() {
 			return {
-				
+				imageURL: "123"
 			}
 		},
 		methods: {
-			
+
 		}
 	}
-</script>
+</script>dred
 
-<style>
-
+<style lang="scss">
+	.bar-plus {
+		color: $uni-text-color-grey !important;
+		margin-left: 15rpx;
+	}
+	.bar-search{
+				color: $uni-text-color-grey !important;
+	}
 </style>
